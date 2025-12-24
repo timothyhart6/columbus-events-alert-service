021f4a0e3131f3d51880afabe6946f1eaaeda508
Timothy Hart <timothyhart6@gmail.com>
2025-12-24 11:33:59 -0500

    Create a refactored Event object, using the builder pattern.

---
+++ src/main/java/com/ColumbusEventAlertService/refactor/models/Event.java
@@ -0,0 +1,74 @@
+package com.ColumbusEventAlertService.refactor.models;
+
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Getter
+public class Event {
+
+    private final String locationName;
+    private final String name;
+    private final LocalDate date;
+    private final String time;
+    private final boolean causesTraffic;
+    private final boolean interesting;
+
+    private Event(Builder builder) {
+        this.locationName = Objects.requireNonNull(builder.locationName);
+        this.name = Objects.requireNonNull(builder.name);
+        this.date = Objects.requireNonNull(builder.date);
+        this.time = builder.time;
+        this.causesTraffic = builder.causesTraffic;
+        this.interesting = builder.interesting;
+    }
+
+    public Builder build() {
+        return new Builder();
+    }
+
+    //Could switch to Lombok builder. Creating manually first to understand how it works under the hood).
+    public static class Builder {
+        private String locationName;
+        private String name;
+        private LocalDate date;
+        private String time;
+        private boolean causesTraffic;
+        private boolean interesting;
+
+        public Builder locationName(String locationName) {
+            this.locationName = locationName;
+            return this;
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder date(LocalDate date) {
+            this.date = date;
+            return this;
+        }
+
+        public Builder time(String time) {
+            this.time = time;
+            return this;
+        }
+
+        public Builder causesTraffic(boolean causesTraffic) {
+            this.causesTraffic = causesTraffic;
+            return this;
+        }
+
+        public Builder interesting(boolean interesting) {
+            this.interesting = interesting;
+            return this;
+        }
+
+        public Event build() {
+            return new Event(this);
+        }
+    }
+}
